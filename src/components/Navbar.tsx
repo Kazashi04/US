2d649979fed05a0beb855958d0bb40117def8f57
Kazashi04 <jlloydpuda365@gmail.com>
2026-06-07 19:20:30 +0800

    Add custom logo to Navbar and Favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -66,6 +66,7 @@ export const Navbar: React.FC<NavbarProps> = ({ onNavigate, onOpenLogin, onOpenS
             handleLinkClick('home');
           }}
         >
+          <img src="/logo.jpg" alt="UniStay Logo" style={{ width: '40px', height: '40px', objectFit: 'contain' }} />
           <span className="logo-text">Uni<span className="logo-accent">Stay</span></span>
         </a>
 
